ed1234a0ddc4462ace8a7925a13641f79a5b25fb
Jeevan <jeevanrpro@gmail.com>
2016-03-13 09:30:40 +0530

    Update PersonController.java

--- Hibernate_G1_Demos/src/com/G1/Java/Eg1/Person/PersonController.java
+++ Hibernate_G1_Demos/src/com/G1/Java/Eg1/Person/PersonController.java
@@ -2,16 +2,15 @@ package com.G1.Java.Eg1.Person;
 
 import java.util.Scanner;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class PersonController {
-	static Scanner scan=new Scanner(System.in);
 
 	public static void main(String[] args) {
-		PersonDTO pdto=new PersonDTO();
 		//Component 1
 		Configuration cfg=new Configuration();
 		cfg.configure();
@@ -23,31 +22,25 @@ public class PersonController {
 
 		//component 1 from component 3
 		Transaction tx=session.beginTransaction();
-		System.out.println("Enter the person name");
-		String pName=scan.next();
-		System.out.println("Enter the gender Male or Female");
-		String pGen=scan.next();
-		char pGene[]=pGen.toCharArray();
-		char pGender=pGene[0];
-		System.out.println("Enter the Bloodgroup");
-		String pBloodGroup=scan.next();
-		System.out.println("Enter the person age");
-		int pAge=scan.nextInt();
-		System.out.println("Enter the person relationship status");
-		String pStatus=scan.next();
+		try{
+			PersonDTO pdto=new PersonDTO();
+			PersonDAO pdao=new PersonDAO();
+			//pdto=pdao.add(pdto);
+			//session.save(pdto);
+			pdto=pdao.update(pdto, session, tx);
+			session.update(pdto);
+			tx.commit();
+		}
+		catch(HibernateException e){
+			e.printStackTrace();
+			tx.rollback();
+		}
+		finally{
+			session.close();
+			factory.close();
 
-		pdto.setpName(pName);
-		pdto.setpGender(pGender);
-		pdto.setpBloodGroup(pBloodGroup);
-		pdto.setpAge(pAge);
-		pdto.setpStatus(pStatus);
-		session.save(pdto);
-		tx.commit();
-		session.close();
-		factory.close();
-		scan.close();
+		}
+
 
 	}
-	
-
 }
